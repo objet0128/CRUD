cc299c1bfd3fe406b6820204eb8bf3e428bf521e
objet0128 <objet.0128@gmail.com>
2022-11-15 15:26:19 +0900

    fix(main.py): fix import name

--- CRUD/main.py
+++ CRUD/main.py
@@ -2,13 +2,14 @@ import uvicorn
 from fastapi import FastAPI
 
 from CRUD.apis.api import api_router
-from CRUD.db.init_db import call_init_db
+from CRUD.db.init_db import init_db
 
-app = FastAPI(callbacks=call_init_db())
+app = FastAPI()
 
 app.include_router(api_router)
 
-MAIN_APP_LOCATION = "CRUD.main:CRUD"
+MAIN_APP_LOCATION = "CRUD.main:app"
 
 if __name__ == "__main__":
+    init_db()
     uvicorn.run(MAIN_APP_LOCATION, host="0.0.0.0", port=8000, reload=True)
