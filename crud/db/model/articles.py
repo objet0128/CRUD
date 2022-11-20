86fd92a66f38905972fc8421d85b72d76e1bb722
objet0128 <objet.0128@gmail.com>
2022-11-20 15:04:28 +0900

    feat(db/model/articles.py): change user_id -> author_id, add lazy="joined" about comments column

--- crud/db/model/articles.py
+++ crud/db/model/articles.py
@@ -9,6 +9,6 @@ class Article(Base, BaseMixin):
 
     title = Column(String(50))
     content = Column(String(255))
-    user_id = Column(Integer, ForeignKey("user.id"))
+    author_id = Column(Integer, ForeignKey("user.id"))
     user = relationship("User", back_populates="articles")
-    comments = relationship("Comment", back_populates="article")
+    comments = relationship("Comment", back_populates="article", lazy="joined")
