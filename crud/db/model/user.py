f3e3d30cada81ab6a2685764f0321e8d3e9ded46
objet0128 <objet.0128@gmail.com>
2022-11-22 15:39:58 +0900

    feat(db/model/user.py): remove email validator

--- crud/db/model/user.py
+++ crud/db/model/user.py
@@ -1,7 +1,5 @@
-import re
-
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, validates
+from sqlalchemy.orm import relationship
 
 from crud.db.base import BaseMixin
 from crud.db.base_class import Base
@@ -13,10 +11,3 @@ class User(Base, BaseMixin):
     information = Column(String)
     articles = relationship("Article", back_populates="user", lazy="joined")
     comments = relationship("Comment", back_populates="user", lazy="joined")
-
-    @validates("email")
-    def validate_email(self, key, value: str):
-        if not re.match(r"[^@]+@[^@]+\.[^@]+", value):
-            raise ValueError("Only email please")
-        else:
-            return value
