cad967af270adc95262414f87cceee712a1ea984
objet0128 <objet.0128@gmail.com>
2022-11-26 15:03:48 +0900

    feat(crud/db/model): remove type hint

--- crud/db/model/comment.py
+++ crud/db/model/comment.py
@@ -7,8 +7,8 @@ from crud.db.base_class import Base
 
 class Comment(Base, BaseMixin):
 
-    comment: str = Column(String(50))
-    user_id: int = Column(Integer, ForeignKey("user.id"))
-    article_id: int = Column(Integer, ForeignKey("article.id"))
+    comment = Column(String(50))
+    user_id = Column(Integer, ForeignKey("user.id"))
+    article_id = Column(Integer, ForeignKey("article.id"))
     user = relationship("User", back_populates="comments")
     article = relationship("Article", back_populates="comments")
